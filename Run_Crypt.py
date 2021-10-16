34fd0bb323530e06f118d311b8d206751ad38bd8
meadscientista <78745171+meadscientista@users.noreply.github.com>
2021-10-16 22:51:00 +0530

    Update Run_Crypt.py

--- Run_Crypt.py
+++ Run_Crypt.py
@@ -41,7 +41,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(ChromeDriverManager().install())
 
 """#### Nerve.Fi"""
 
